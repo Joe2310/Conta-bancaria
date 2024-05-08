0442608376fe960ffc5a87a700a8f80f571b4159
Joe Santos <joe.ssa @hotmail.com>
2024-05-08 17:13:27 -0300

    Cadastro de cliente.

---
+++ Conta Banco/src/ContaTerminal.java
@@ -0,0 +1,11 @@
+import usuario.cpf12345678990.NovaConta;
+
+public class ContaTerminal {
+    public static void main(String[] args){
+        
+        NovaConta conta = new NovaConta();
+        
+        conta.Usuario();
+      
+    }
+}
