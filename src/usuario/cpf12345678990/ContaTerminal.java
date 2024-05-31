ab8a832a8b494e123029e32dbaa89fd70ff979f1
Joe santos <133579879+Joe2310@users.noreply.github.com>
2024-05-31 00:36:18 -0300

    Add files via upload

---
+++ src/usuario/cpf12345678990/ContaTerminal.java
@@ -0,0 +1,10 @@
+package usuario.cpf12345678990;
+public class ContaTerminal {
+    public static void main(String[] args){
+        
+        NovaConta conta = new NovaConta();
+        
+        conta.Usuario();
+      
+    }
+}
